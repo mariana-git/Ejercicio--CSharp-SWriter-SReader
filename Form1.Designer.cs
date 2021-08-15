52c3ebd698b92503e57847577fe3d4c8521cf58a
Mariana <marianasayagosistemas@gmail.com>
2021-08-15 17:29:42 -0300

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace sRsW_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLeer = new System.Windows.Forms.Button();
+            this.btnEscribir = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnLeer
+            // 
+            this.btnLeer.BackColor = System.Drawing.SystemColors.ControlText;
+            this.btnLeer.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnLeer.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnLeer.Location = new System.Drawing.Point(34, 117);
+            this.btnLeer.Name = "btnLeer";
+            this.btnLeer.Size = new System.Drawing.Size(187, 40);
+            this.btnLeer.TabIndex = 0;
+            this.btnLeer.Text = "Leer Chistes";
+            this.btnLeer.UseVisualStyleBackColor = false;
+            this.btnLeer.Click += new System.EventHandler(this.btnLeer_Click);
+            // 
+            // btnEscribir
+            // 
+            this.btnEscribir.BackColor = System.Drawing.SystemColors.ControlText;
+            this.btnEscribir.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnEscribir.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnEscribir.Location = new System.Drawing.Point(34, 437);
+            this.btnEscribir.Name = "btnEscribir";
+            this.btnEscribir.Size = new System.Drawing.Size(187, 40);
+            this.btnEscribir.TabIndex = 1;
+            this.btnEscribir.Text = "Grabar Chiste";
+            this.btnEscribir.UseVisualStyleBackColor = false;
+            this.btnEscribir.Click += new System.EventHandler(this.btnEscribir_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(34, 178);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(591, 231);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(214, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(204, 40);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Chistes Malos";
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.BackColor = System.Drawing.SystemColors.ControlText;
+            this.btnAgregar.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnAgregar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnAgregar.Location = new System.Drawing.Point(438, 117);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(187, 40);
+            this.btnAgregar.TabIndex = 5;
+            this.btnAgregar.Text = "Agregar Chistes";
+            this.btnAgregar.UseVisualStyleBackColor = false;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.BackColor = System.Drawing.SystemColors.ControlText;
+            this.btnSalir.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnSalir.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSalir.Location = new System.Drawing.Point(438, 437);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(187, 40);
+            this.btnSalir.TabIndex = 6;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = false;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(664, 489);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnEscribir);
+            this.Controls.Add(this.btnLeer);
+            this.Name = "Form1";
+            this.Text = "SR y SW";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLeer;
+        private System.Windows.Forms.Button btnEscribir;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace sRsW_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            btnEscribir.Visible = false;
+        }
+
+        private void btnLeer_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            StreamReader sr = new StreamReader(".\\chistes.txt");
+            string linea = sr.ReadToEnd();
+            richTextBox1.AppendText(linea);
+            sr.Close();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            btnEscribir.Visible = true;
+            btnAgregar.Visible = false;
+            btnLeer.Visible = false;
+        }
+
+        private void btnEscribir_Click(object sender, EventArgs e)
+        {
+            StreamWriter sw = new StreamWriter(".\\chistes.txt", true);
+            sw.WriteLine(richTextBox1.Text);
+            sw.Close();
+
+            richTextBox1.Clear();
+            btnEscribir.Visible = false;
+            btnAgregar.Visible = true;
+            btnLeer.Visible = true;
+
+            MessageBox.Show("El chiste fue agregado a la colección!", "GRACIAS");
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Dispose();
+        }
+    }
+}
